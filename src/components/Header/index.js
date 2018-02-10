e1b69ed6d14114de5c93d74c08a0d4ca88d6d791
Josh Prince <josh.prince@arcadiasolutions.com>
2018-02-10 15:43:34 -0600

    Style header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,13 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './index.scss'
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <header>
+    <Link to="/">
+      {<span className="font-weight-600">Josh Prince</span>} | Design & Development
+    </Link>
+  </header>
 )
 
 export default Header
