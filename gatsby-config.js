ba884b13742be1b11471fbc14d31a25967a04a20
Josh Prince <josh.prince@arcadiasolutions.com>
2018-03-23 22:40:03 -0400

    Add manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,30 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        author: 'Josh Prince',
+        name: "jprince.codes",
+        short_name: "jprince.codes",
+        start_url: "/",
+        background_color: "#36b0d8",
+        theme_color: "#36b0d8",
+        display: "standalone",
+        icons: [
+          {
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-sharp`,
